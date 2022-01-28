b0c7f0cc8e4c5e9417b4812ba1d72d9d9567ba46
Giordano Migliorini Estevão <gestevao04@gmail.com>
2022-01-28 19:46:48 -0300

    Add files via upload

---
+++ src/java/com/giordano/exposeproducts/entity/ProductEntity.java
@@ -0,0 +1,42 @@
+package com.giordano.exposeproducts.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+//@Table(schema = "COMPANY", name = "PRODUCT")
+@Getter @Setter
+public class ProductEntity {
+    @Id
+    @Column(name = "ID")
+    String id;
+
+    @Column(name = "NAME")
+    String name;
+
+    @Column(name = "CATEGORY")
+    String category;
+
+    @Column(name = "URL")
+    String url;
+
+    @Column(name = "ICON_URL")
+    String iconUrl;
+
+    @Column(name = "PRICE")
+    Double price;
+
+    @Column(name = "QUANTITY_SOLD")
+    Integer qttSold;
+
+    @Column(name = "AVALIBLE_STOCK")
+    Integer avalibleStock;
+
+    @Column(name = "CREATED_DATE")
+    LocalDateTime createdDate;
+}
