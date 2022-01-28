07acf804125b4a0d0be32eb6fe5151b514598dcb
Giordano Migliorini Estevão <gestevao04@gmail.com>
2022-01-28 19:45:18 -0300

    Add files via upload

---
+++ src/java/com/giordano/exposeproducts/ExposeProductsApplication.java
@@ -0,0 +1,13 @@
+package com.giordano.exposeproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposeProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposeProductsApplication.class, args);
+	}
+
+}
